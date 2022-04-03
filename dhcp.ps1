c47507151d1f2fbba48f3ad02ca3202c62c1bf94
felipe8398 <felipe-p-s2011@hotmail.com>
2022-04-03 04:51:01 -0300

    Update dhcp.ps1

--- dhcp.ps1
+++ dhcp.ps1
@@ -1,3 +1,3 @@
-$IP=$args[1]
+param ($IP)
 write-host "Buscando pelo IP/RANGE/Nome da máquina: $IP"
 Get-Content C:\Windows\System32\dhcp\DhcpSrvLog-*.log | Where-Object {$_ -like '*assign*'} | select-string $IP | ConvertFrom-Csv -Header ID,Data, Hora, Ação, IP, Maquina | Format-Table -Autosize
